f0a406d81afc3dc1f90f69f7329744e5883000f0
mynna404 <gemingjia0201@163.com>
2025-08-15 00:19:27 +0800

    build: 更新构建配置并添加发布插件

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
+    id("tech.medivh.plugin.publisher") version "1.2.5"
     kotlin("jvm") version "2.2.0"
 }
 
-group = "org.kinetide.fmtprint"
-version = "0.0.1-SNAPSHOT"
+group = "io.github.mynna404"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
@@ -13,7 +14,6 @@ dependencies {
     implementation("com.google.code.gson:gson:2.13.1")
     testImplementation("org.junit.jupiter:junit-jupiter:5.13.4")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-
 }
 
 tasks.test {
@@ -21,4 +21,32 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(17)
+}
+
+medivhPublisher{
+    groupId = project.group.toString()
+    artifactId = project.name
+    version = project.version.toString()
+    pom{
+        name = "format-print"
+        description = "format-print is a tool designed to provide Java and Kotlin developers with a more user-friendly and structured way to print object contents."
+        url = "https://github.com/mynna404/format-print"
+        licenses{
+            license{
+                name = "Apache-2.0 license"
+                url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+            }
+        }
+        developers {
+            developer {
+                id = "mynna404"
+                name = "mynna404"
+                email = "gemingjia0201@163.com"
+            }
+        }
+        scm {
+            connection = "scm:git:"
+            url = "https://github.com/mynna404/format-print"
+        }
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,13 @@
+rootProject.name = "format-print"
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
-rootProject.name = "format-print"
